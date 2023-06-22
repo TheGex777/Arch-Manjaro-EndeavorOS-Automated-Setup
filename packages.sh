3ca5f739ec852b7041c005205b62c5a9361d506b
TheGex777 <80421683+TheGex777@users.noreply.github.com>
2023-06-22 13:03:26 -0400

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Define the packages you want to install
+packages=(
+  virtualbox
+  clang
+  cmake
+  ninja
+  gparted
+  git
+  audacious
+  neofetch
+  bleachbit
+  cherrytree
+  vlc
+  bleachbit
+  emacs
+  jre17-openjdk
+  jdk17-openjdk
+  ufw
+  gufw
+  libreoffice-fresh
+  nodejs
+  npm
+  onlyoffice-bin
+)
+
+# Check if yay is installed, if not, install it
+if ! command -v yay > /dev/null 2>&1; then
+  echo "yay not found. Installing yay..."
+  sudo pacman -S --needed --noconfirm yay
+fi
+
+# Update package lists and upgrade system
+sudo pacman -Syu --noconfirm
+
+# Install packages
+for package in "${packages[@]}"
+do
+  if pacman -Qs "$package" > /dev/null; then
+    echo "Package $package is already installed. Skipping..."
+  else
+    if ! pacman -Si "$package" > /dev/null 2>&1; then
+      echo "Unable to find package $package in main repository. Using yay instead."
+      yay -S "$package" --noconfirm
+    else
+      echo "Installing package $package..."
+      sudo pacman -S "$package" --noconfirm
+    fi
+  fi
+done
